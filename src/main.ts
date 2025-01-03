00582bb7a8df0dbf3088d9f46c9ba7779ba55f42
Alexander Halsey <49983070+AlexanderHalsey@users.noreply.github.com>
2025-01-03 18:14:22 +0100

    add threejs background with rubix cube

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import './app/three/three';
+
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
