5d540cd1f9dbd627ba26f1120c4493172d6e7d28
Alexander Halsey <alex.halsey5@gmail.com>
2025-01-08 00:40:37 +0100

    add reactive form to contact section

--- src/app/sections/contact/contact.component.ts
+++ src/app/sections/contact/contact.component.ts
@@ -1,5 +1,14 @@
-import { Component, computed, Signal } from '@angular/core';
+import { Component, computed, signal, Signal } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common';
+import {
+  AbstractControl,
+  ControlEvent,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  StatusChangeEvent,
+  Validators,
+} from '@angular/forms';
 
 import { ButtonComponent } from '@/components/button/button.component';
 import { IconComponent } from '@/components/icon/icon.component';
@@ -13,19 +22,87 @@ import { Theme } from '@/models';
 
 @Component({
   selector: 'app-contact',
-  imports: [ButtonComponent, IconComponent, InputComponent, NgTemplateOutlet, TextAreaComponent],
+  imports: [
+    ButtonComponent,
+    IconComponent,
+    InputComponent,
+    NgTemplateOutlet,
+    ReactiveFormsModule,
+    TextAreaComponent,
+  ],
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
   isMobile: Signal<boolean>;
   theme: Signal<Theme>;
   backgroundColor: Signal<string>;
+
+  form: FormGroup;
+
   constructor(
     private displayService: DisplayService,
     private themeService: ThemeService,
+    private formBuilder: FormBuilder,
   ) {
     this.isMobile = displayService.get('isMobile');
     this.theme = themeService.get('theme');
     this.backgroundColor = computed(() => themeService.backgroundColors().background2);
+
+    this.form = formBuilder.group(
+      {
+        firstName: ['', [Validators.required]],
+        lastName: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        phone: [''],
+        company: [''],
+        subject: ['', [Validators.required]],
+        message: ['', [Validators.required]],
+      } satisfies Record<keyof FormData, unknown>,
+      { updateOn: 'blur' },
+    );
+    (Object.entries(this.form.controls) as [keyof FormData, AbstractControl][]).forEach(
+      ([key, control]) => {
+        control.events.subscribe(() => {
+          const error: string | undefined = Object.keys(control.errors ?? {})[0];
+          this.errorBag.update((existing) => ({
+            ...existing,
+            [key]: ERROR_MESSAGES[error] ?? error,
+          }));
+        });
+      },
+    );
+    this.form.events.subscribe((e: ControlEvent<FormData>) => {
+      if (!(e instanceof StatusChangeEvent)) return;
+      if (e.status === 'VALID') {
+        this.submitDisabled.set(false);
+      }
+    });
+  }
+
+  submitDisabled = signal(false);
+  errorBag = signal<Partial<Record<keyof FormData, string>>>({});
+
+  async onSubmit() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      console.log('valid');
+    } else {
+      this.submitDisabled.set(true);
+    }
   }
 }
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  company: string;
+  subject: string;
+  message: string;
+}
+
+export const ERROR_MESSAGES: Record<string, string> = {
+  required: $localize`Required`,
+  email: $localize`Invalid email`,
+};
--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,4 +1,6 @@
-import { Component, input, Signal } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, forwardRef, HostListener, input, model, Signal } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { ThemeService } from '@/services/theme.service';
 
@@ -7,13 +9,49 @@ import { Theme } from '@/models';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
+  value = model<string>('');
+
+  onChange = (_value: string) => {};
+  onTouched = () => {};
+
   type = input<'text' | 'email' | 'tel'>('text');
   placeholder = input<string>('');
+  error = input<string>();
 
   theme: Signal<Theme>;
   constructor(themeService: ThemeService) {
     this.theme = themeService.get('theme');
+  }
+
+  onInput(event: Event) {
+    this.value.set((event.target as HTMLInputElement).value);
+    this.onChange(this.value());
+  }
+
+  writeValue(value: string) {
+    if (value === null) return;
+    this.value.set(value);
+  }
+
+  registerOnChange(fn: (value: string) => void) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void) {
+    this.onTouched = fn;
+  }
+
+  @HostListener('focusout')
+  onFocusOut() {
+    this.onTouched();
   }
 }
