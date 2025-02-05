e062d671000542dfc963538834a568a895d722c1
Alexander Halsey <49983070+AlexanderHalsey@users.noreply.github.com>
2025-02-05 11:03:10 +0100

    improve light-up mechanism for tech stack component

--- src/app/sections/tech-stack/tech-stack.component.ts
+++ src/app/sections/tech-stack/tech-stack.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, computed, OnDestroy, signal } from '@angular/core';
+import { AfterViewInit, Component, computed, signal } from '@angular/core';
 
 import { IconComponent } from '@/components/icon/icon.component';
 import { TooltipComponent } from '@/components/tooltip/tooltip.component';
@@ -13,81 +13,65 @@ import { IconName } from '@/models';
   templateUrl: './tech-stack.component.html',
   styleUrl: './tech-stack.component.css',
 })
-export class TechStackComponent implements AfterViewInit, OnDestroy {
-  techStack = [
-    { icon: 'angular', name: 'Angular' },
-    { icon: 'azure-devops', name: 'Azure DevOps' },
-    { icon: 'css', name: 'CSS' },
-    { icon: 'cypress', name: 'Cypress' },
-    { icon: 'django', name: 'Django' },
-    { icon: 'docker', name: 'Docker' },
-    { icon: 'git', name: 'Git' },
-    { icon: 'html', name: 'HTML' },
-    { icon: 'javascript', name: 'Javascript' },
-    { icon: 'jest', name: 'Jest' },
-    { icon: 'nest', name: 'Nest' },
-    { icon: 'node', name: 'Node' },
-    { icon: 'npm', name: 'Npm' },
-    { icon: 'postgresql', name: 'PostgreSQL' },
-    { icon: 'python', name: 'Python' },
-    { icon: 'react', name: 'React' },
-    { icon: 'sass', name: 'Sass' },
-    { icon: 'tailwindcss', name: 'Tailwind CSS' },
-    { icon: 'typescript', name: 'Typescript' },
-    { icon: 'vite', name: 'Vite' },
-    { icon: 'vue', name: 'Vue' },
-  ] satisfies { icon: IconName; name: string }[];
-
-  techStackGroups = computed<{ icon: IconName; name: string }[][]>(() => {
-    const groupLength = 7;
-    const techStack = this.techStack;
-    return Array.from(
-      { length: Math.ceil(techStack.length / groupLength) },
-      (_, index) => index * groupLength,
-    ).map((begin, index) => {
+export class TechStackComponent implements AfterViewInit {
+  techStacks = signal<TechStackItem[][]>(
+    Array.from({ length: 3 }, () => TECH_STACK.map((item) => ({ ...item, hovered: false }))),
+  );
+  // stagger the tech stack items so that each bar shows a different set of items
+  getBegininingIndex = (index: number) => index * 7;
+  techStackGroups = computed<TechStackItem[][]>(() =>
+    this.techStacks().map((techStack, index) => {
+      const begin = this.getBegininingIndex(index);
       const stack = techStack.slice(begin).concat(techStack.slice(0, begin));
       return index % 2 === 0 ? stack : stack.reverse();
-    });
-  });
+    }),
+  );
 
-  hoveredItem = signal<TechStackItem | null>(null);
-  setHoveredItem(item: TechStackItem) {
-    return this.hoveredItem.set(item);
-  }
-  removeHoveredItem(item: TechStackItem) {
-    if (this.hoveredItem() === item) {
-      this.hoveredItem.set(null);
-    }
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  scrollObserverDisconnect = () => {};
   ngAfterViewInit() {
-    this.scrollObserverDisconnect = scrollObserver('#tech-stack', {
-      enterCallback: (_, index) => {
-        this.techStackGroups()[index].forEach((techStackItem, index) => {
-          setTimeout(() => {
-            const hoveredItem = this.hoveredItem();
-            if (hoveredItem) this.removeHoveredItem(hoveredItem);
-            this.setHoveredItem(techStackItem);
-          }, 200 * index);
-        });
-        setTimeout(() => {
-          const hoveredItem = this.hoveredItem();
-          if (hoveredItem) this.removeHoveredItem(hoveredItem);
-          this.scrollObserverDisconnect();
-        }, 200 * this.techStackGroups()[index].length);
-      },
-      options: { threshold: 0.9 },
+    this.techStacks().forEach((techStack, stackIndex) => {
+      const animationTimeMs = 100;
+      const begin = this.getBegininingIndex(stackIndex);
+      const disconnectObserver = scrollObserver(`.tech-stack-item-${stackIndex}`, {
+        enterCallback: () => {
+          techStack.forEach((item, index) => {
+            const i = (techStack.length + index - begin) % techStack.length;
+            setTimeout(() => (item.hovered = true), i * animationTimeMs);
+            setTimeout(() => (item.hovered = false), (i + 7) * animationTimeMs);
+          });
+          setTimeout(() => disconnectObserver(), techStack.length * 2 * animationTimeMs);
+        },
+        options: { threshold: 1 },
+      });
     });
-  }
-
-  ngOnDestroy() {
-    this.scrollObserverDisconnect();
   }
 }
 
 interface TechStackItem {
   icon: IconName;
   name: string;
+  hovered: boolean;
 }
+
+const TECH_STACK = [
+  { icon: 'angular', name: 'Angular' },
+  { icon: 'azure-devops', name: 'Azure DevOps' },
+  { icon: 'css', name: 'CSS' },
+  { icon: 'cypress', name: 'Cypress' },
+  { icon: 'django', name: 'Django' },
+  { icon: 'docker', name: 'Docker' },
+  { icon: 'git', name: 'Git' },
+  { icon: 'html', name: 'HTML' },
+  { icon: 'javascript', name: 'Javascript' },
+  { icon: 'jest', name: 'Jest' },
+  { icon: 'nest', name: 'Nest' },
+  { icon: 'node', name: 'Node' },
+  { icon: 'npm', name: 'Npm' },
+  { icon: 'postgresql', name: 'PostgreSQL' },
+  { icon: 'python', name: 'Python' },
+  { icon: 'react', name: 'React' },
+  { icon: 'sass', name: 'Sass' },
+  { icon: 'tailwindcss', name: 'Tailwind CSS' },
+  { icon: 'typescript', name: 'Typescript' },
+  { icon: 'vite', name: 'Vite' },
+  { icon: 'vue', name: 'Vue' },
+] satisfies { icon: IconName; name: string }[];
