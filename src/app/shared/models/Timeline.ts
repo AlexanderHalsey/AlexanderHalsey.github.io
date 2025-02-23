e00150e23b14d536df26dcc73c7d79edc2e96811
Alexander Halsey <49983070+AlexanderHalsey@users.noreply.github.com>
2025-02-23 14:41:35 +0100

    add timeline milestone type

--- src/app/shared/models/Timeline.ts
+++ src/app/shared/models/Timeline.ts
@@ -1,5 +1,15 @@
-export interface TimelineItem {
+interface BaseTimelineItem {
   startDate: Date;
   endDate?: Date;
+  includeToggleBtn?: boolean;
+}
+export interface TimelineMilestone extends BaseTimelineItem {
+  milestone: string;
+}
+
+export interface TimelineDateRange extends BaseTimelineItem {
+  title: string;
   description: string;
 }
+
+export type TimelineItem = TimelineDateRange | TimelineMilestone;
