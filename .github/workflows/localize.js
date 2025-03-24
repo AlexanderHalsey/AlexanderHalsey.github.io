16c577cc972574a6c7c0253ed681d5e924a3ca24
Alexander Halsey <alex.halsey5@gmail.com>
2025-03-24 15:36:55 +1030

    entry file as html for initialization

--- .github/workflows/localize.js
+++
@@ -1,8 +0,0 @@
-export const languages = ['en', 'fr', 'es']
-
-if (location.pathname === '/') {
-  location.href =
-    location.origin +
-    '/' +
-    (languages.find((language) => language === navigator.language.slice(0, 2)) ?? 'en')
-}
